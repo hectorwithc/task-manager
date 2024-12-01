bce3ba2c27886a1b226ffe7a9320b5a335e0a5a4
Hector <102716847+hectorwithc@users.noreply.github.com>
2024-12-01 15:17:31 +0100

    Update favicon and app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { TRPCReactProvider } from "~/trpc/react";
-import { APP_NAME } from "~/lib/appConfig";
+import { APP_LOGO_NAME } from "~/lib/appConfig";
 import { Toaster } from "~/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: `${APP_NAME}`,
-  description: "A online task manager",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  title: `${APP_LOGO_NAME}`,
+  description: "An online task manager.",
+  icons: [{ rel: "icon", url: "/favicon.svg" }],
 };
 
 export default function RootLayout({
